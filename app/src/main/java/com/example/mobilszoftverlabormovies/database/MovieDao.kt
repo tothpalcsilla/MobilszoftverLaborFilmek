536649617784291a414190bd184ff736f0b3b19c
Tóthpál Csilla <tothpalcsilla@gmail.com>
2022-05-15 23:55:05 +0200

    database layer

--- app/src/main/java/com/example/mobilszoftverlabormovies/database/MovieDao.kt
+++ app/src/main/java/com/example/mobilszoftverlabormovies/database/MovieDao.kt
@@ -1,7 +1,10 @@
 package com.example.mobilszoftverlabormovies.database
 
+import androidx.room.*
 import com.example.mobilszoftverlabormovies.model.Movie
+import retrofit2.http.DELETE
 
+@Dao
 interface MovieDao {
     // leírja az adatbázis kéréseket
 
@@ -9,21 +12,37 @@ interface MovieDao {
     // Premierek
     // Népszerűek
     // Most játszottak
+
+    @Query("SELECT * FROM Movie")
     fun getAllMovies(): List<Movie>
+
+    @Query("SELECT * FROM Movie")
     fun getLatestMovies(): List<Movie>
+
+    @Query("SELECT * FROM Movie")
     fun getPopularMovies(): List<Movie>
+
+    @Query("SELECT * FROM Movie")
     fun getNowPlayingMovies(): List<Movie>
 
+    @Query("SELECT * FROM Movie WHERE id = :movie_id")
     fun getMovie(movie_id: String): Movie
 
     // CREATE
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertMovie(movie: Movie): Long
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertMoviesList(movies: List<Movie>): List<Long>
 
     // POST
+    @Update(onConflict = OnConflictStrategy.REPLACE)
     fun updateMovie(movie: Movie)
 
     // DELETE
-    fun deleteMovie(movie: Movie)
+    @Query("DELETE FROM Movie WHERE id = :movie_id")
+    fun deleteMovie(movie_id: String)
+
+    @Query("DELETE FROM Movie")
     fun deleteAllMovies()
 }
--- app/src/main/java/com/example/mobilszoftverlabormovies/di/DatabaseModule.kt
+++ app/src/main/java/com/example/mobilszoftverlabormovies/di/DatabaseModule.kt
@@ -1,7 +1,10 @@
 package com.example.mobilszoftverlabormovies.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.mobilszoftverlabormovies.database.AppDatabase
 import com.example.mobilszoftverlabormovies.database.MovieDao
-import com.example.mobilszoftverlabormovies.model.Movie
+import com.example.mobilszoftverlabormovies.R
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,4 +15,22 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
+    @Provides
+    @Singleton
+    fun provideAppDatabase(application: Application): AppDatabase {
+        return Room
+            .databaseBuilder(
+                application,
+                AppDatabase::class.java,
+                application.getString(R.string.database)
+            )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePosterDao(appDatabase: AppDatabase): MovieDao {
+        return appDatabase.movieDao()
+    }
 }
